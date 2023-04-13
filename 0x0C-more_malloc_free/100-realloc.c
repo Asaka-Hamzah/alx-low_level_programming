e3874090c7b9a87c3b7ee7a4639612610ceef090
Asaka Hamzah-Mohammed Awinebisah <asakahamzahawinebisah@gmail.com>
2023-04-13 13:41:41 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  * @new_size: new size of the new memory block
  *
  * Return: pointer to the newly allocated memory block
- * @Author: Asaka Hamzah-Mohammed Awinebisah
+ * Author: Asaka Hamzah-Mohammed Awinebisah
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
